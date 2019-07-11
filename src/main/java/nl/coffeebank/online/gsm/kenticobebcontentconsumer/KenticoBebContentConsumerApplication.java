e5d69559b92186b6427b6c68bea81653cffbfc72
prabudossrs <prabudoss.ramachandriya.santharam@rabobank.nl>
2019-07-11 11:56:36 +0200

    Initial commit

---
+++ src/main/java/nl/coffeebank/online/gsm/kenticobebcontentconsumer/KenticoBebContentConsumerApplication.java
@@ -0,0 +1,13 @@
+package nl.coffeebank.online.gsm.kenticobebcontentconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenticoBebContentConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenticoBebContentConsumerApplication.class, args);
+	}
+
+}
